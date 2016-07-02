28d6670e829f595f5c1e69b4ead5163dcf2b35d6
Donovan Hutchinson <donovan@kitmanlabs.com>
2016-07-02 18:21:46 +0100

    Moving logic into a furby module

--- refurb/README.md
+++ refurb/README.md
@@ -23,3 +23,15 @@ Possible related uses:
 
 * Gold box filtered deal alerts
 * ... other online retailers with affiliate schemes?
+
+## Add to Slack button
+
+Follow tutorial: https://99designs.ie/tech-blog/blog/2015/08/26/add-to-slack-button/
+
+* Set up a subdomain pointing to the DO server
+* Add it to nginx config - dedicated port for this app
+* When url works - set up landing page / installation page
+* Do post from app on app registration
+* Confirm it's integrated and test bot
+* Give bot some custom logic to save user - webhook so that it can message user
+* Test messaging user with custom info
---
+++ refurb/lib/furby/messaging.js
@@ -0,0 +1,49 @@
+'use strict';
+
+function postNewResultsToChannel(bot, results) {
+  //console.log(results.length + ' new results found');
+  bot.postMessageToChannel('refurbs', 'New item added to the refurb page', buildAttachments(result, '#764FA5'));
+}
+
+function postMatchedResultsToUser(bot, results, user, keywords) {
+  results.forEach(function(result) {
+    if (isMatch(result.matchText, keywords)) {
+      // TODO: Build a createMessage function that introduces more pertinent info / personality
+      // Also, set the colour different for each product
+      bot.postMessageToUser(user, 'I found a match for you on the Apple refurb store!', buildAttachments(result, '#96E23E'));
+    }
+  });
+}
+function buildAttachments(result, color) {
+  let text = '*' + result.price + '*\n';
+  text += result.description;
+  return {
+    "attachments": [{
+      "title": result.title,
+      "title_link": result.link,
+      "text": text,
+      "image_url": result.image,
+      "color": color
+    }]
+  };
+}
+function buildRegex(keywords) {
+  // ^(?=.*\bmeat\b)(?=.*\bpasta\b)(?=.*\bdinner\b).+
+  let regex = '';
+  keywords.forEach(function(keyword) {
+    regex += '(?=.*'+ keyword +')'
+  });
+  regex += '.+';
+  return regex;
+}
+
+function isMatch(matchText, keywords) {
+  // Return true if this matches keywords
+  let regex = buildRegex(keywords);
+  return matchText.search(regex) > -1 ? true : false;
+}
+
+module.exports = {
+  postNewResultsToChannel: postNewResultsToChannel,
+  postMatchedResultsToUser: postMatchedResultsToUser
+};
