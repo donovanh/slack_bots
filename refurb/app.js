1b8a5b8beabd4337eb7ba86c49a16ba24582783f
Donovan Hutchinson <donovanh+git@gmail.com>
2016-07-18 23:56:07 +0100

    Adding user stuff

--- refurb/app.js
+++ refurb/app.js
@@ -48,7 +48,7 @@ const express = require('express'),
       cookieParser = require('cookie-parser'),
       bodyParser = require('body-parser'),
       routes = require('./routes/index'),
-      users = require('./routes/users'),
+      users = require('./lib/users'),
       app = express();
 
 // view engine setup
@@ -64,7 +64,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/users', users); // Users module returns routes prefixed with '/users/*'
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ refurb/lib/users/index.js
@@ -0,0 +1,43 @@
+/* A users module that presents a signup flow and saves the organisations, users and bots for the app */
+'use strict';
+
+const routes = require('routes'),
+      express = require('express'),
+      app = express();
+
+const express = require('express'),
+      router = express.Router(),
+      request = require('request');
+
+// Routes
+router.get('/installed', function(req, res, next) {
+  let code = req.query.code,
+      client_id = process.env.client_id,
+      client_secret = process.env.client_secret;
+  let url = 'https://slack.com/api/oauth.access?code=' + code + '&client_id=' + client_id + "&client_secret=" + client_secret;
+  request(url, function (err, res, body) {
+    if (err) {
+      console.log('Error: ', err);
+    }
+    let response = JSON.stringify(body);
+    // Response
+    /*
+      {"ok":true,"access_token":"xoxp-15550051014-15550051062-56509410246-fde319a3b1",
+      "scope":"identify,bot,incoming-webhook","user_id":"U0FG61H1U","team_name":"donovanh",
+      "team_id":"T0FG61H0E","incoming_webhook":{"channel":"#refurbs","channel_id":"C1MPWH42G",
+      "configuration_url":"https:\/\/donovanh.slack.com\/services\/B1NFFA8JD",
+      "url":"https:\/\/hooks.slack.com\/services\/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"},
+      "bot":{"bot_user_id":"U1NFJCCEN","bot_access_token":"xoxb-56528420498-4FeRCdxnbCtZfsmP5X02S4Sd"}}
+    */
+    if (response.ok) {
+      // Set up the user locally
+      console.log('Response: ', response);
+      // Redirect to a confirmation page
+    } else {
+      // Redirect to error page
+    }
+  });
+
+});
+
+module.exports = router;
