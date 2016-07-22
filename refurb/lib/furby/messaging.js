f6db5e3b41451dd2ccd78f8f4507c06368340203
Donovan Hutchinson <donovanh+git@gmail.com>
2016-07-22 19:49:14 +0100

    Debugging

--- refurb/lib/furby/messaging.js
+++ refurb/lib/furby/messaging.js
@@ -2,7 +2,7 @@
 
 function postNewResultsToChannel(bot, results) {
   console.log(results.length + ' new results found');
-  bot.postMessageToChannel('dev', '', buildAttachments(result[0], '#764FA5'));
+  bot.postMessageToChannel('dev', 'text', '');
   results.forEach(function(result) {
     bot.postMessageToChannel('dev', '', buildAttachments(result, '#764FA5'));
   }); 
