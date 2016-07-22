aed649804d99644c9dda5ef9bb64179a8ff6d55e
Donovan Hutchinson <donovanh+git@gmail.com>
2016-07-22 20:37:45 +0100

    testing local message

--- refurb/lib/furby/messaging.js
+++ refurb/lib/furby/messaging.js
@@ -2,7 +2,7 @@
 
 function postNewResultsToChannel(bot, results) {
   console.log(results.length + ' new results found');
-  results.forEach(function(index, result) {
+  results.forEach(function(result) {
     bot.postMessageToChannel('dev', 'A new item was found on the Irish refurb store!', buildAttachments(result, '#764FA5'));
   }); 
 }
